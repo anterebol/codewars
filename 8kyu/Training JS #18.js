9b224f401f988073416d971c092d0d0bd7a9f3e3
unknown <anterebol@gmail.com>
2023-06-01 11:06:16 +0300

    feat: add 8kyu folder for easiest task from codewars and add desicion for some tasks

---
+++ 8kyu/Training JS #18.js
@@ -0,0 +1,14 @@
+// Task
+// Implement a function which accepts 2 arguments: string and separator.
+
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+const splitAndMerge = (string, separator) => {
+  return string
+    .split(" ")
+    .map((item) => {
+      if (item) return item.split("").join(separator);
+      return item;
+    })
+    .join(" ");
+};
