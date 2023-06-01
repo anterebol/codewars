082d823051d10e2d42681f68dfd86045a7752003
unknown <anterebol@gmail.com>
2023-06-02 00:54:15 +0300

    feat: add solutions for 7kyu tasks

---
+++ 7kyu/Set Reducer.js
@@ -0,0 +1,43 @@
+// link: https://www.codewars.com/kata/63cbe409959401003e09978b/train/javascript
+// Intro
+// These arrays are too long! Let's reduce them!
+
+// Description
+// Write a function that takes in an array of integers from 0-9, and returns a new array:
+
+// Numbers with no identical numbers preceding or following it returns a 1: 2, 4, 9  => 1, 1, 1
+// Sequential groups of identical numbers return their count: 6, 6, 6, 6 => 4
+// Example
+
+// [0, 4, 6, 8, 8, 8, 5, 5, 7] => [1, 1, 1, 3, 2, 1]
+
+// Your function should then repeat the process on the resulting array, and on the resulting array of that, until it returns a single integer:
+
+// [0, 4, 6, 8, 8, 8, 5, 5, 7] =>  [1, 1, 1, 3, 2, 1] => [3, 1, 1, 1] => [1, 3] => [1, 1] => [2]
+
+// When your function has reduced the array to a single integer following these rules, it should return that integer.
+
+// [2] => 2
+
+// Rules and assertions
+// All test arrays will be 2+ in length
+// All integers in the test arrays will be positive numbers from 0 - 9
+// You should return an integer, not an array with 1 element
+
+function setReducer(input) {
+  while (input.length > 1) {
+    let d = 1;
+    for (let i = 0; i < input.length; i++) {
+      for (let k = i + 1; k < input.length; k++) {
+        if (input[i] === input[k]) {
+          d++;
+        } else {
+          break;
+        }
+      }
+      input.splice(i, d, d);
+      d = 1;
+    }
+  }
+  return input[0];
+}
