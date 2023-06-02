d981b1cb3f7ed320f4d017f953b2174e352d50c8
unknown <anterebol@gmail.com>
2023-06-03 02:32:01 +0300

    feat: add solutions for 6kyu katas

---
+++ 6kyu/Back and forth then Reverse.js
@@ -0,0 +1,46 @@
+// link: https://www.codewars.com/kata/60cc93db4ab0ae0026761232/train/javascript
+// Task
+// A list S will be given. You need to generate a list T from it by following the given process:
+
+// Remove the first and last element from the list S and add them to the list T.
+// Reverse the list S
+// Repeat the process until list S gets emptied.
+// The above process results in the depletion of the list S. Your task is to generate list T without mutating the input List S.
+
+// Example
+// S = [1,2,3,4,5,6]
+// T = []
+
+// S = [2,3,4,5] => [5,4,3,2]
+// T = [1,6]
+
+// S = [4,3] => [3,4]
+// T = [1,6,5,2]
+
+// S = []
+// T = [1,6,5,2,3,4]
+// return T
+// Note
+// size of S goes up to 10^6
+// Keep the efficiency of your code in mind.
+// Do not mutate the Input.
+
+function arrange(s) {
+  const t = [];
+  const c = s.length / 2;
+  let g = true;
+  for (let i = 1; i < c + 0.5; i++) {
+    if (g) {
+      t.push(s[i - 1]);
+      t.push(s[s.length - i]);
+    } else {
+      t.push(s[s.length - i]);
+      t.push(s[i - 1]);
+    }
+    g = !g;
+  }
+  if (c > Math.floor(c)) {
+    t.push(s[c - 0.5]);
+  }
+  return t;
+}
