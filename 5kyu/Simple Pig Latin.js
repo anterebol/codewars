079cf0e38f673cd74623cc664b6f20cf91a5df9e
unknown <anterebol@gmail.com>
2023-06-04 01:08:45 +0300

    feat: add solutions for 5kyu katas

---
+++ 5kyu/Simple Pig Latin.js
@@ -0,0 +1,13 @@
+// link: https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+const pigIt = (str) =>
+  str
+    .split(" ")
+    .map((item) =>
+      !item.match(/[^\w]/) ? item.slice(1) + item[0] + "ay" : item
+    )
+    .join(" ");
